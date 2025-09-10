d4398012beec23e4a2e1d7d7f6994fb1234c1d76
Rafiqul Islam <rafibd500@gmail.com>
2025-09-11 03:09:12 +0600

    starter 203 added

---
+++ Codechef/Starters 203/Passing_Grade.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fastIO() ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+#define endl '\n'
+#define int long long
+#define pb push_back
+#define all(x) x.begin(), x.end()
+#define YES cout << "YES" << endl
+#define NO cout << "NO" << endl
+#define el cout << endl
+/*-------------------------End of Macros-------------------------*/
+
+const int mod = 1e9+7;
+void solution()
+{
+    int n; cin>>n;
+    int x; cin>>x;
+    n--;
+    int cnt = 1;
+    while(n--){
+        int a; cin>>a;
+        if(a>=x) cnt++;
+    }
+
+
+    cout<<cnt<<endl;
+}
+
+signed main()
+{
+    fastIO();
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solution();
+    }
+}
