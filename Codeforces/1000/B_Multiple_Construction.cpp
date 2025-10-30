540a01643fb914c6b7b9eb17aa348d4be7c92e30
Rafiqul Islam <rafibd500@gmail.com>
2025-10-30 17:15:43 +0600

    updated

---
+++ Codeforces/1000/B_Multiple_Construction.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fastIO() ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+#define endl '\n'
+#define int long long
+#define pb push_back
+#define all(x) x.begin(), x.end()
+#define YES cout << "YES" << endl
+#define NO cout << "NO" << endl
+#define el cout << endl
+/*-------------------------End of Macros-------------------------*/
+
+const int mod = 1e9+7;
+void solution()
+{
+    int n; cin>>n;
+    for(int i=n; i>=1; i--){
+        cout<<i<<" ";
+    }
+    cout<<n<<" ";
+    for(int i=1; i<n; i++){
+        cout<<i<<" ";
+    }
+    cout<<endl;
+}
+
+signed main()
+{
+    fastIO();
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solution();
+    }
+}
